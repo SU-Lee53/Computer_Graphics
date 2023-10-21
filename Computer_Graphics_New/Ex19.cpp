4576a1390514a3ecd81fcb15c2a30598a593c077
SU-Lee53 <dltmddnr05@naver.com>
2023-10-21 19:55:33 +0900

    19번 행 성 시 뮬 완 성

--- Computer_Graphics_New/Ex19.cpp
+++ Computer_Graphics_New/Ex19.cpp
@@ -12,7 +12,7 @@ Ex19::~Ex19()
 
 void Ex19::InitEx()
 {
-	_worldMat = GET_SINGLE(TransformManager).GetRotateMatrix(10.0f, X_AXIS);
+	_worldMat = GET_SINGLE(TransformManager).GetRotateMatrix(20.0f, X_AXIS);
 	_orbit1 = glm::mat4(1.0f);
 	_orbit2 = GET_SINGLE(TransformManager).GetRotateMatrix(45.0f, Z_AXIS);
 	_orbit3 = GET_SINGLE(TransformManager).GetRotateMatrix(-45.0f, Z_AXIS);
@@ -20,17 +20,28 @@ void Ex19::InitEx()
 	_parentMat1 = GET_SINGLE(TransformManager).GetTranslateMatrix(glm::vec3(3.0f, 0.0f, 0.0f));
 	_parentMat2 = GET_SINGLE(TransformManager).GetTranslateMatrix(glm::vec3(0.0f, 0.0f, -3.0f));
 	_parentMat3 = GET_SINGLE(TransformManager).GetTranslateMatrix(glm::vec3(0.0f, 0.0f, 3.0f));
+	
+	_childMat1 = GET_SINGLE(TransformManager).GetTranslateMatrix(glm::vec3(4.0f, 0.0f, 0.0f));
+	_childMat2 = GET_SINGLE(TransformManager).GetTranslateMatrix(glm::vec3(0.0f, 0.0f, -4.0f));
+	_childMat3 = GET_SINGLE(TransformManager).GetTranslateMatrix(glm::vec3(0.0f, 0.0f, 4.0f));
+	
+	_lastMat1 = GET_SINGLE(TransformManager).GetTranslateMatrix(glm::vec3(1.0f, 0.0f, 0.0f));
+	_lastMat2 = GET_SINGLE(TransformManager).GetTranslateMatrix(glm::vec3(0.0f, 0.0f, -1.0f));
+	_lastMat3 = GET_SINGLE(TransformManager).GetTranslateMatrix(glm::vec3(0.0f, 0.0f, 1.0f));
+
+	_scaleMat1 = GET_SINGLE(TransformManager).GetScaleMatrix(glm::vec3(0.4f, 0.4f, 0.4f));
+	_scaleMat2 = GET_SINGLE(TransformManager).GetScaleMatrix(glm::vec3(0.2f, 0.2f, 0.2f));
 
 	_camera = new Camera(glm::vec3(0.0, 0.0, 2.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
-	_projection = new Projection(45.0f, 1.0f, 0.1f, 50.0f, -5.0f);
+	_projection = new Projection(45.0f, 1.0f, 0.1f, 50.0f, -8.0f);
 }
 
 void Ex19::drawScene()
 {
 	_camera->Bind();
 	_projection->Bind();
-	yRot = GET_SINGLE(TransformManager).GetRotateMatrix(yDeg, Y_AXIS);
-	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * yRot);
+	
+	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * yRot1);
 	glEnable(GL_COLOR_MATERIAL);
 
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
@@ -51,7 +62,7 @@ void Ex19::Reshape(int w, int h)
 
 void Ex19::Render()
 {
-	if (_orbit == nullptr)
+	if (_bigOrbit == nullptr)
 	{
 		SetOrbit();
 		SetPlanet();
@@ -62,8 +73,15 @@ void Ex19::Render()
 	
 
 
+	yRot1 = GET_SINGLE(TransformManager).GetRotateMatrix(yDeg1, Y_AXIS);
+	yDeg1 += 0.05f;
+	
+	yRot2 = GET_SINGLE(TransformManager).GetRotateMatrix(yDeg2, Y_AXIS);
+	yDeg2 += 0.03f;
+	
+	yRot3 = GET_SINGLE(TransformManager).GetRotateMatrix(yDeg3, Y_AXIS);
+	yDeg3 += 0.07f;
 
-	yDeg += 0.05f;
 }
 
 void Ex19::SetOrbit()
@@ -73,42 +91,57 @@ void Ex19::SetOrbit()
 	while (degree <= 360.0f)
 	{
 		Coord pos = { 0.0f, 0.0f, 0.0f };
-		pos.x = _orbitRadius * glm::cos(glm::radians(degree));
-		pos.z = _orbitRadius * glm::sin(glm::radians(degree));
+		pos.x = _bigOrbitRadius * glm::cos(glm::radians(degree));
+		pos.z = _bigOrbitRadius * glm::sin(glm::radians(degree));
 		
-		_orbitVBuf[idx] = pos.x;
-		_orbitVBuf[idx + 1] = pos.y;
-		_orbitVBuf[idx + 2] = pos.z;
+		_bigOrbitVBuf[idx] = pos.x;
+		_bigOrbitVBuf[idx + 1] = pos.y;
+		_bigOrbitVBuf[idx + 2] = pos.z;
 
-		_orbitCBuf[idx] = 0.0f;
-		_orbitCBuf[idx + 1] = 0.0f;
-		_orbitCBuf[idx + 2] = 0.0f;
+		_bigOrbitCBuf[idx] = 0.0f;
+		_bigOrbitCBuf[idx + 1] = 0.0f;
+		_bigOrbitCBuf[idx + 2] = 0.0f;
 
-		degree += 5.0f;
+		degree += 10.0f;
 		idx += 3;
 	}
 
-	_orbit = new VAO(_orbitVBuf, _orbitCBuf, idx);
+	_bigOrbit = new VAO(_bigOrbitVBuf, _bigOrbitCBuf, idx);
 
+	degree = 0.0f;
+	idx = 0;
+	while (degree <= 360.0f)
+	{
+		Coord pos = { 0.0f, 0.0f, 0.0f };
+		pos.x = _smallOrbitRadius * glm::cos(glm::radians(degree));
+		pos.z = _smallOrbitRadius * glm::sin(glm::radians(degree));
+
+		_smallOrbitVBuf[idx] = pos.x;
+		_smallOrbitVBuf[idx + 1] = pos.y;
+		_smallOrbitVBuf[idx + 2] = pos.z;
+
+		_smallOrbitCBuf[idx] = 0.0f;
+		_smallOrbitCBuf[idx + 1] = 0.0f;
+		_smallOrbitCBuf[idx + 2] = 0.0f;
+
+		degree += 10.0f;
+		idx += 3;
+	}
+
+	_smallOrbit = new VAO(_smallOrbitVBuf, _smallOrbitCBuf, idx);
 }
 
 void Ex19::SetPlanet()
 {
 	// 1. 조상: 중심점, 부모행성 3개를 자식으로 가짐
-	_ancestor = new Objects(QOBJ_SPHERE, {0.0f, 0.0f, 0.0f}, 1.0f, GLU_LINE, {0.0f, 0.0f, 1.0f});
-
-	// 2. 부모: 조상 주변을 돌고, 자식 행성을 하나씩 가짐
-	_parent1 = new Objects(QOBJ_SPHERE, { 0.0f, 0.0f, 0.0f }, 0.3f, GLU_LINE, { 0.0f, 0.0f, 1.0f });
-	_parent2 = new Objects(QOBJ_SPHERE, { 0.0f, 0.0f, 0.0f }, 0.3f, GLU_LINE, { 0.0f, 0.0f, 1.0f });
-	_parent3 = new Objects(QOBJ_SPHERE, { 0.0f, 0.0f, 0.0f }, 0.3f, GLU_LINE, { 0.0f, 0.0f, 1.0f });
-
+	_planet = new Objects(QOBJ_SPHERE, {0.0f, 0.0f, 0.0f}, 1.0f, GLU_LINE, {0.0f, 0.0f, 1.0f});
 
 }
 
 void Ex19::RenderOrbit()
 {
-	glBindVertexArray(_orbit->GetVAOHandle());
-	int bufSize = _orbit->GetVertexCount();
+	glBindVertexArray(_bigOrbit->GetVAOHandle());
+	int bufSize = _bigOrbit->GetVertexCount();
 
 	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * _orbit1);
 	for (int i = 0; i < (bufSize / 3) - 1; i++)
@@ -127,23 +160,61 @@ void Ex19::RenderOrbit()
 	{
 		glDrawArrays(GL_LINES, i, 2);
 	}
+	
+	glBindVertexArray(_smallOrbit->GetVAOHandle());
+	bufSize = _smallOrbit->GetVertexCount();
 
+	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * yRot2 * _parentMat1);
+	for (int i = 0; i < (bufSize / 3) - 1; i++)
+	{
+		glDrawArrays(GL_LINES, i, 2);
+	}
+	
+	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * (_orbit2 * yRot2) * _parentMat2);
+	for (int i = 0; i < (bufSize / 3) - 1; i++)
+	{
+		glDrawArrays(GL_LINES, i, 2);
+	}
+	
+	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * (_orbit3 * yRot2) * _parentMat3);
+	for (int i = 0; i < (bufSize / 3) - 1; i++)
+	{
+		glDrawArrays(GL_LINES, i, 2);
+	}
 
 }
 
 void Ex19::RenderPlanet()
 {
-	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * _ancestorMat * yRot);
-	_ancestor->Render();
+	glm::mat4 currMat = _worldMat * _ancestorMat * yRot1;
+	GET_SINGLE(TransformManager).BindTransformMatrix(currMat);
+	_planet->Render();
 	
-	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * yRot * _parentMat1 * yRot);
-	_parent1->Render();											 
+	// 부모;
+	currMat = _worldMat * (_orbit1 * yRot2) * (_parentMat1 * yRot1) * _scaleMat1;
+	GET_SINGLE(TransformManager).BindTransformMatrix(currMat);
+	_planet->Render();
 
-	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * (_orbit2 * yRot) * _parentMat2 * yRot);
-	_parent2->Render();											 
+	currMat = _worldMat * (_orbit2 * yRot2) * (_parentMat2 * yRot1) * _scaleMat1;
+	GET_SINGLE(TransformManager).BindTransformMatrix(currMat);
+	_planet->Render();
 
-	GET_SINGLE(TransformManager).BindTransformMatrix(_worldMat * (_orbit3 * yRot) * _parentMat3 * yRot);
-	_parent3->Render();
+	currMat = _worldMat * (_orbit3 * yRot2) * (_parentMat3 * yRot1) * _scaleMat1;
+	GET_SINGLE(TransformManager).BindTransformMatrix(currMat);
+	_planet->Render();
+	
+	// 자식
+	currMat = _worldMat * (_orbit1 * yRot2) * _parentMat1 * (yRot3) * (_lastMat1 * yRot1) * _scaleMat2;
+	GET_SINGLE(TransformManager).BindTransformMatrix(currMat);
+	_planet->Render();
+
+	currMat = _worldMat * (_orbit2 * yRot2) * _parentMat2 * (yRot3) * (_lastMat2 * yRot1) * _scaleMat2;
+	GET_SINGLE(TransformManager).BindTransformMatrix(currMat);
+	_planet->Render();
+
+	currMat = _worldMat * (_orbit3 * yRot2) * _parentMat3 * (yRot3) * (_lastMat3 * yRot1) * _scaleMat2;
+	GET_SINGLE(TransformManager).BindTransformMatrix(currMat);
+	_planet->Render();
 
 
 
--- Computer_Graphics_New/Ex19.h
+++ Computer_Graphics_New/Ex19.h
@@ -32,37 +32,56 @@ private:
 	Camera* _camera;
 	glm::mat4 _worldMat = glm::mat4(1.0);
 
-	VAO* _orbit = nullptr;
-	float _orbitRadius = 3.0f;
-	float _orbitVBuf[1000];
-	float _orbitCBuf[1000];
+	// 큰 궤도 -> 부모 행성들이 도는 경로
+	VAO* _bigOrbit = nullptr;
+	float _bigOrbitRadius = 3.0f;
+	float _bigOrbitVBuf[150];
+	float _bigOrbitCBuf[150];
 
-	// 경로에 종속될 변환행렬
+	// 작은 궤도 -> 자식 행성들이 도는 경로
+	VAO* _smallOrbit = nullptr;
+	float _smallOrbitRadius = 1.0f;
+	float _smallOrbitVBuf[400];
+	float _smallOrbitCBuf[400];
+
+	// 경로에 대한 변환행렬
 	glm::mat4 _orbit1;
 	glm::mat4 _orbit2;
 	glm::mat4 _orbit3;
 
+	// 행성: 조상1, 부모3, 자식3(트리처럼)
+	// 조상 하나 만들고 나머지는 신축해서 만듬
+	Objects* _planet;
+	glm::mat4 _scaleMat1 = glm::mat4(1.0f);
+	glm::mat4 _scaleMat2 = glm::mat4(1.0f);
 
-	// 행성들: 조상1, 부모3, 자식3(트리처럼)
-	Objects* _ancestor;
-	Objects* _parent1;
-	Objects* _parent2;
-	Objects* _parent3;
-	Objects* _child1;
-	Objects* _child2;
-	Objects* _child3;
 
 	// 헹성 변환행렬
 	glm::mat4 _ancestorMat = glm::mat4(1.0f);
+
 	glm::mat4 _parentMat1 = glm::mat4(1.0f);
 	glm::mat4 _parentMat2 = glm::mat4(1.0f);
 	glm::mat4 _parentMat3 = glm::mat4(1.0f);
-	glm::mat4 _chlidMat1 = glm::mat4(1.0f);
-	glm::mat4 _chlidMat2 = glm::mat4(1.0f);
-	glm::mat4 _chlidMat3 = glm::mat4(1.0f);
+
+	glm::mat4 _childMat1 = glm::mat4(1.0f);
+	glm::mat4 _childMat2 = glm::mat4(1.0f);
+	glm::mat4 _childMat3 = glm::mat4(1.0f);
+	
+	// 공전의 공전 -> 궤도만큼 더 이동
+	glm::mat4 _lastMat1 = glm::mat4(1.0f);
+	glm::mat4 _lastMat2 = glm::mat4(1.0f);
+	glm::mat4 _lastMat3 = glm::mat4(1.0f);
 
 
-	glm::mat4 yRot = glm::mat4(1.0f);
-	float yDeg = 0.0f;
+	glm::mat4 yRot1 = glm::mat4(1.0f);
+	float yDeg1 = 0.0f;
+
+	glm::mat4 yRot2 = glm::mat4(1.0f);
+	float yDeg2 = 0.0f;
+	
+	glm::mat4 yRot3 = glm::mat4(1.0f);
+	float yDeg3 = 0.0f;
+
+
 };
 
